2b69e12e0d728f6a4ce1104b3633267c01937654
PramodGit66 <ahirwarpramodkumar15@gmail.com>
2024-09-26 15:43:13 +0530

    initial commit in master branch

---
+++ src/test/java/com/micro/serviceRegistory/SbmsServiceResistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.serviceRegistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsServiceResistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
